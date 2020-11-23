b6f24f02930cdc4d6a28356d9db99610d1651ef2
elze <elze.hamilton@gmail.com>
2020-11-23 09:32:07 -0600

    Adding SkillsDataService, because I need custom URL for entity collection, because ngrx incorrectly appends a slash at the end of the standard URL

---
+++ src/app/skills/skills-data.service.ts
@@ -0,0 +1,20 @@
+import { DefaultDataService, DefaultDataServiceConfig, DefaultHttpUrlGenerator, DefaultPluralizer } from "@ngrx/data";
+import { HttpClient } from '@angular/common/http';
+import { StructSkill } from "./models/struct-skill";
+
+export class SkillsDataService extends DefaultDataService<StructSkill> {
+  private defaultDataServiceConfig: DefaultDataServiceConfig;
+  constructor(http: HttpClient, 
+    defaultDataServiceConfig: DefaultDataServiceConfig) {
+    const urlGenerator = new DefaultHttpUrlGenerator(new DefaultPluralizer([]));
+    //const url = new DefaultHttpUrlGenerator();
+    const urlRoot = defaultDataServiceConfig.root;  
+    const resourceUrls = {
+      entityResourceUrl: `${defaultDataServiceConfig.root}/structskill`,
+      collectionResourceUrl: `${defaultDataServiceConfig.root}/structskills`
+      };
+      urlGenerator.registerHttpResourceUrls({['StructSkill']: resourceUrls});
+      super('StructSkill', http, urlGenerator);
+      this.defaultDataServiceConfig = defaultDataServiceConfig;
+  }
+}
